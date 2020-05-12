81a08fd15af4065e0da7d24ca971dbf58b0bc50a
robbie <>
2020-05-12 20:20:59 +0800

    add test dag

--- mysqlhook.py
+++ mysqlhook.py
@@ -1,9 +1,8 @@
 import airflow
 from airflow.operators.python_operator import PythonOperator
 from airflow.hooks.mysql_hook import MySqlHook
+from airflow.utils.log.logging_mixin import LoggingMixin, StreamLogWriter, set_context
 import logging
-import os
-
 
 main_dag_id = 'mysqlhook'
 args = {
@@ -18,6 +17,9 @@ dag = airflow.DAG(
     default_args=args,
 )
 
+log = StreamLogWriter(logger=logging, level=logging.ERROR)
+
+
 def doTestMysqlHook(*args, **kwargs):
     sql_hook = MySqlHook().get_hook(conn_id="mysql_operator_test_connid")
     sql = "select * from manzeng_predict_src_table;"
@@ -29,7 +31,7 @@ def doTestMysqlHook(*args, **kwargs):
     print('maxid:' + str(result[0][0]))
     result = sql_hook.get_first(sql)
     print('maxid:' + str(result[0]))
-    logging.critical('log test err')
+    log.write("err log test")
     sql_hook.run("""insert into manzeng_result_v3(consignor_phone,prediction) values('122','33')""")
 
 doMysqlTask = PythonOperator(
